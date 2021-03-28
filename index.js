2ced44e60cecd4c51c339670e83617691e437222
Rahul Gupta <64692111+RahulGupta899@users.noreply.github.com>
2021-03-29 00:22:36 +0530

    All files added

---
+++ index.js
@@ -0,0 +1,20 @@
+var randomNumber1 = Math.random();
+randomNumber1 = randomNumber1 * 6;
+randomNumber1 = Math.floor(randomNumber1) + 1; // range : 1 -6
+
+var randomNumber2 = Math.random();
+randomNumber2 = randomNumber2 * 6;
+randomNumber2 = Math.floor(randomNumber2) + 1; // range : 1 -6
+
+var img = document.querySelectorAll("img");
+img[0].setAttribute("src", "images/dice" + randomNumber1 + ".png");
+img[1].setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+var heading = document.querySelector("h1");
+if (randomNumber1 === randomNumber2) {
+  heading.textContent = "Draw!";
+} else if (randomNumber1 > randomNumber2) {
+  heading.textContent = "🚩Player 1 wins!";
+} else {
+  heading.textContent = "Player2 wins!🚩";
+}
